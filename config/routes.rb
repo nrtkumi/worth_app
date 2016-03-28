308ae660680cf0a29dc72d06e2a80cbee2adb8f4
TakumiNara <takuminara0428@gmail.com>
2016-03-28 16:14:40 +0900

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  resources :users, only: [:index, :show, :edit, :update]
   root 'top#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
