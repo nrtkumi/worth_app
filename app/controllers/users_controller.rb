f95b6f6cbacefe54ef638f439855d4cf8b681084
TakumiNara <takuminara0428@gmail.com>
2016-03-30 11:18:10 +0900

    auth user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  include ApplicationHelper
+
+  before_action :authenticate_user!, only:[:show, :edit, :update]
+  before_action :correct_user, only:[:edit, :update]
+
   def index
     @users = User.all
   end
@@ -25,5 +30,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :job, :gender, :introduction)
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    if !current_user?(user)
+      redirect_to root_path
+    end
   end
 end
