fbc56db2d76cea9a4cd0f3366c10e1567ec1cf11
TakumiNara <takuminara0428@gmail.com>
2016-08-06 01:23:33 +0900

    error page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get 'chat-list', to: 'projects#chatlist'
   get 'new_projects' => 'projects#new_projects'
   get 'pick_up' => 'projects#pick_up'
+  get "bad_request" => "top#bad_request"
+  get "internal_server_error" => "top#internal_server_error"
   root 'top#home'
-
+  match "*anything" => "top#not_found", via: [:get, :post, :patch, :delete]
 end
